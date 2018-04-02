f227e10ff1f88e4b244f1677257f78df6a2de2cf
Will Heim <wheim42@gmail.com>
2018-04-02 10:03:12 -0500

    view model added

---
+++ PersonalBlog/PersonalBlog.UI/Models/PostSubmissionVM.cs
@@ -0,0 +1,16 @@
+﻿using PersonalBlog.Models.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PersonalBlog.UI.Models
+{
+    public class PostSubmissionVM
+    {
+        string Title { get; set; }
+        string Body { get; set; }
+        List<Category> Categories { get; set; }
+        List<Tag> Tags { get; set; }
+    }
+}
